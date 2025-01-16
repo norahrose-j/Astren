12569df1c6c1523f08787722c6fb1fef28ed9cb1
norahrose-j <norah.jurdjevic@gmail.com>
2025-01-16 10:28:53 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+
+window.addEventListener('scroll', function () {
+    const parallax = document.querySelector('.parallax');
+    let scrollPosition = window.pageYOffset;
+    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
+
+window.addEventListener('load', function () {
+    const parallax = document.querySelector('.parallax');
+    let scrollPosition = window.pageYOffset;
+    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
+
+window.addEventListener('scroll', function () {
+    const subparallax = document.querySelector('.subparallax');
+    let scrollPosition = window.pageYOffset;
+    subparallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
+
+window.addEventListener('load', function () {
+    const subparallax = document.querySelector('.subparallax');
+    let scrollPosition = window.pageYOffset;
+    subparallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+})
+
+function changeColorClick(element) {
+    element.addEventListener('click', function() {
+        element.style.color = '#000';
+    })
+}
+
+var mapPopUp = document.getElementById('ashariamap');
+mapPopUp.addEventListener('click', function () {
+    const popUpImg = document.getElementById('ashariapopup');
+    popUpImg.style.display = 'block';
+})
+
+var closePopUp = document.getElementById('ashariaxmark'); 
+closePopUp.style.fontSize = '75px'
+closePopUp.addEventListener('click', function () {
+    const popUpImg = document.getElementById('ashariapopup');
+    popUpImg.style.display = 'hidden';
+})
