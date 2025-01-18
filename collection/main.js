439376916cf9ea8e07f14651b8c6e288f56f3c85
norahrose-j <norah.jurdjevic@gmail.com>
2025-01-18 11:47:22 -0500

    added type filter for collection

--- collection/main.js
+++ collection/main.js
@@ -5,14 +5,82 @@ const statblocks = document.getElementsByClassName('statblock');
 
 for (let i = 0; i < pluses.length; i++) {
     const element = pluses[i];
-    const clist = element.classList;
+    const cList = element.classList;
 
     // rotate plus into x on click
     element.addEventListener('click', function () {
-        clist.toggle('intox');
+        cList.toggle('intox');
 
         // show stablock ?? please
-        const statlist = statblocks[i].classList;
-        statlist.toggle('showstats');
-    })
-}
+        const statList = statblocks[i].classList;
+        statList.toggle('showstats');
+    });
+};
+
+// FILTER BY TYPE
+const npc = document.getElementById('npcfilter');
+const monster = document.getElementById('monsterfilter');
+const equip = document.getElementById('equipfilter');
+const magic = document.getElementById('magicfilter');
+
+const entries = document.getElementsByClassName('entry');
+
+npc.addEventListener('click', function () {
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
+        const eList = entry.classList;
+
+        if (eList.contains("npc")) {
+            eList.add("showentry")
+            eList.remove("hideentry")
+        } else {
+            eList.add("hideentry")
+            eList.remove("showentry")
+        }
+    }
+});
+
+monster.addEventListener('click', function () {
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
+        const eList = entry.classList;
+
+        if (eList.contains("monster")) {
+            eList.add("showentry")
+            eList.remove("hideentry")
+        } else {
+            eList.add("hideentry")
+            eList.remove("showentry")
+        }
+    }
+});
+
+equip.addEventListener('click', function () {
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
+        const eList = entry.classList;
+
+        if (eList.contains("equip")) {
+            eList.add("showentry")
+            eList.remove("hideentry")
+        } else {
+            eList.add("hideentry")
+            eList.remove("showentry")
+        }
+    }
+});
+
+magic.addEventListener('click', function () {
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
+        const eList = entry.classList;
+
+        if (eList.contains("magic")) {
+            eList.add("showentry")
+            eList.remove("hideentry")
+        } else {
+            eList.add("hideentry")
+            eList.remove("showentry")
+        }
+    }
+});
