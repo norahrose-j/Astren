0ae15e046477076ce863f78958e4b71b36cbd6d8
norahrose-j <norah.jurdjevic@gmail.com>
2025-01-18 11:24:35 -0500

    added rotate animation for x click

---
+++ collection/main.js
@@ -0,0 +1,13 @@
+
+// SHOW / HIDE STATBLOCK
+const pluses = document.getElementsByClassName('fa-plus');
+
+for (let i = 0; i < pluses.length; i++) {
+    const element = pluses[i];
+    const clist = element.classList;
+
+    // rotate plus into x on click
+    element.addEventListener('click', function () {
+        clist.toggle('intox');
+    })
+}
